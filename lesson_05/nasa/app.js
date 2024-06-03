a150780da3d9f64b181c72040a1f353dafdd192b
sully <spo8698@gmail.com>
2024-06-03 09:56:37 -0400

    update to nasa files in lesson_05

--- lesson_05/nasa/app.js
+++ lesson_05/nasa/app.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', getFetch);
 const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
-
-fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+// api key yF5P7muYQuWPROCRclCcJdtGk65LCjY7sV3SR7LB
+fetch('https://api.nasa.gov/planetary/apod?api_key=yF5P7muYQuWPROCRclCcJdtGk65LCjY7sV3SR7LB')
     .then(res => res.json())
     .then(data => {
         console.log(data)
@@ -11,6 +11,12 @@ async function getFetch() {
     const dateChoice = document.querySelector('input').value;
     const data = await fetch(url+`&date=${dateChoice}`);
     const info = await data.json();
-    console.log(info)
-    document.querySelector('img').src = info.url
+    console.log(info);
+    if (info.media_type === "image"){
+        document.querySelector('img').src = info.url;
+    } else {
+        document.querySelector('h2').innerHTML = "image type is a video, third party cookies may have been blocked";
+        document.querySelector('img').alt = info.url;
+        document.querySelector('img').src = info.url;
+    }
 }  
